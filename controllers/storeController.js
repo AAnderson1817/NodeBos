8ee98ac54fe8fed179cd6fe54a3593e06b600d09
Andy Anderson <andyanderson1818@gmail.com>
2017-07-05 15:30:57 -0700

    Adding stores

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose');
 const Store = mongoose.model('Store');
 
 exports.homePage = (req, res) => {
-  console.log(req.name);
   res.render('index');
 };
 
@@ -12,7 +11,12 @@ exports.addStore = (req, res) => {
 
 exports.createStore = async (req, res) => {
   const store = await (new Store(req.body)).save();
-  await store.save();
-  req.flash('success', `Successfully created ${store.name}. Care to leave a review?`);
+  req.flash('success', `Successfully Created ${store.name}. Care to leave a review?`);
   res.redirect(`/store/${store.slug}`);
+};
+
+exports.getStores = async (req, res) => {
+  // 1. Query the database for a list of all stores
+  const stores = await Store.find();
+  res.render('stores', { title: 'Stores', stores });
 };
