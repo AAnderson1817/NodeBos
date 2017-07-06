6cc001324d47a58e0ab7b4cf2f65e0c9686ddc67
Andy Anderson <andyanderson1818@gmail.com>
2017-07-06 15:28:27 -0700

    Added individual store routes and views

--- routes/index.js
+++ routes/index.js
@@ -22,4 +22,5 @@ catchErrors(storeController.updateStore)
 
 router.get('/stores/:id/edit', catchErrors(storeController.editStore));
 
+router.get('/store/:slug', catchErrors(storeController.getStoreBySlug))
 module.exports = router;
