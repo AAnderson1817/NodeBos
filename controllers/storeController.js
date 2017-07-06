91a743421c550a00af0b56780be0d3d9c0c65b22
Andy Anderson <andyanderson1818@gmail.com>
2017-07-06 13:48:47 -0700

    Images upload and resize

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -1,6 +1,9 @@
 const mongoose = require('mongoose');
 const Store = mongoose.model('Store');
 const multer = require('multer');
+const jimp = require('jimp');
+const uuid = require('uuid');
+
 const multerOptions = {
   storage: multer.memoryStorage(),
   fileFilter(req, file, next) {
@@ -20,9 +23,27 @@ exports.homePage = (req, res) => {
 exports.addStore = (req, res) => {
   res.render('editStore', { title: 'Add Store' });
 };
-
+//Select a single photo to upload with multer
 exports.upload = multer(multerOptions).single('photo');
-
+//Resize the image and pass along
+exports.resize = async (req, res, next) => {
+  //Check if there is a file
+  if (!req.file){
+    next(); //If there isn't a file,skip to next middleware
+    return;
+  }
+  //To get the correct file type, we grab the mimetype rather than user extension. We split something like image/jpeg and grab the second half (jpeg), which is what we need.
+  const extension = req.file.mimetype.split('/')[1];
+  //Assign each photo a unique id using the uuid middleware.
+  req.body.photo = `${uuid.v4()}.${extension}}`;
+  //Now, resize!
+  const photo = await jimp.read(req.file.buffer);
+  await photo.resize(800, jimp.AUTO);
+  await photo.write(`./public/uploads/${req.body.photo}`);
+  //Once we write the photo to our file system, keep going.
+  next();
+};
+//Create store using the parameters sent in the body of the request, a.k.a input fields.
 exports.createStore = async (req, res) => {
   const store = await (new Store(req.body)).save();
   req.flash('success', `Successfully Created ${store.name}. Care to leave a review?`);
