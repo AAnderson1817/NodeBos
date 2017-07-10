9c4887b4d37cacec5703beb93417d1423c394a12
Andy Anderson <andyanderson1818@gmail.com>
2017-07-10 11:56:28 -0700

    Began creating users and validation

---
+++ models/User.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+mongoose.Promise = global.Promise;
+const md5 = require('md5');
+const validator = require('validator');
+const mongodbErrorHandler = require('mongoose-mongodb-errors');
+const passportLocalMongoose = require('password-local-mongoose');
+
+const userSchema = new Schema({
+  email: {
+    type: String,
+    unique: true,
+    lowercase: true,
+    trim: true,
+    validate: [validator.isEmail, "Invalid Email Address"],
+    required: "Please enter your email"
+  }
+  name:{
+    type:String,
+    required: "Please enter your name"
+    trim:true
+  }
+})
+
+
+
+
+module.exports = mongoose.model('User', userSchema);
