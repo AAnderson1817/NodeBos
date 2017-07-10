436ad95bb4cb88bc7096077219bb310bf2690c85
Andy Anderson <andyanderson1818@gmail.com>
2017-07-10 13:12:29 -0700

    Validated registration and login

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const storeController = require('../controllers/storeController');
 const userController = require('../controllers/userController');
+const authController = require('../controllers/authController');
 const { catchErrors } = require('../handlers/errorHandlers');
 
 router.get('/', catchErrors(storeController.getStores));
@@ -35,7 +36,8 @@ router.get('/register', userController.registerForm);
 //Finally, log them in.
 router.post('/register',
   userController.validateRegister,
-  userController.register
+  userController.register,
+  authController.login
 );
 
 
